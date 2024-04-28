def4883256fb641dec304a1d5f9529f5c86d49c4
Andrey Gorkaviy <fvvcvjfbhhf@gmail.com>
2024-04-28 19:15:54 +0300

    Creating project

--- src/server.js
+++ src/server.js
@@ -1,16 +1,35 @@
 import express from 'express';
 import fs from 'fs';
-import { userRouter } from './routes/userRouter.js';
-import {ErrorHandler} from './middleware/ErrorHandler.js';
+import {processVideo} from './uploadVideoFunction.js'
+
 
 const app = express();
 app.use(express.json());
 
-app.use('/user', userRouter);
 
 
 
 
-app.use(ErrorHandler);
+app.post('/upload', async (req, res) => {
+    try {
+        await processVideo(req);
+        res.json({
+            status: 200,
+            message: 'Відео успішно завантажено!'
+        });
+    } catch (err) {
+        res.json(
+            {
+                status: 500,
+                errorMessage: "Помилка завантаження відео."
+            }
+        )
+    }
+});
+
+
+
+
+
 
 export { app };
